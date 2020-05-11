0d7c73b4c0984bab4632a6736fecbfe6b010120b
flavien-hugs <geekactux@gmail.com>
2020-05-11 13:57:03 +0000

    update models

--- app/models.py
+++ app/models.py
@@ -12,9 +12,9 @@ class Entreprise(models.Model):
     name = models.CharField("Nom de l'entreprise", max_length=30, unique=True)
     director = models.CharField("Directeur Général", default='Mr/Mme', max_length=30)
     domaine = models.CharField("Domaine d'activité", max_length=50)
-    email = models.EmailField('Adresse email', unique=True)
+    email = models.EmailField('Adresse email', unique=True, blank=True)
     contact = models.CharField('Contact', default='+225 00 00 00 00', max_length=50)
-    link = models.URLField('Site internet', default='https://', unique=True, blank=True)
+    link = models.URLField('Site internet', default='https://', blank=True)
     location = models.CharField('Lieu', max_length=50)
 
     class Meta:
